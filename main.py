04afbbc3bed46030923ac3c64413dc8eaf152aad
Michael Krause <mekva6274@gmail.com>
2015-11-17 17:19:55 -0500

    Updated to Ion
    Now works with Ion, v3 of TJHSST's Intranet.

--- main.py
+++ main.py
@@ -1,20 +1,9 @@
 #!/usr/bin/env python
 #
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 #
 #(c) 2015 MICHAEL KRAUSE, TJHSST CLASS OF 2018
+#
+#Now works with Ion!
 #
 import json
 import webapp2
@@ -31,21 +20,24 @@ def timeToInt(formTime):
 
 
 def getSchedule():
-        response = urllib2.urlopen('https://ion.tjhsst.edu/schedule/embed?iodine')
+        response = urllib2.urlopen('https://ion.tjhsst.edu/schedule/embed')
         html = response.read()
         table = html[html.find('<table class="bellschedule-table"'):html.find("</table>")]
-        pdCount = table.count("<tr>")
+        table = table[table.find('>'):]
+        pdCount = table.count("<tr ")
         periodNames = []
         periodNames.append(pdCount)
         for x in range (0, pdCount):
-           tmpPd = table[table.find("<th>")+4:table.find("</th>")]
-           tmpTime = table[table.find("<td>")+4:table.find("</td>")]
-           tmpStart = tmpTime[0:tmpTime.find(" - ")]
-           tmpEnd = tmpTime[tmpTime.find(" - ")+3:]
-           periodNames.append(dict([('name', tmpPd.replace(":", "")), ('startForm', tmpStart), ('endForm', tmpEnd), ('startInt', timeToInt(tmpStart)), ('endInt', timeToInt(tmpEnd))]))
+           tmpPd = table[table.find("<tr ")+4:table.find('">')]
+           name = tmpPd[tmpPd.find("data-block-name=")+17:tmpPd.find("data-block-start=")-2]
+           name = name.strip()
+           tmpStart = tmpPd[tmpPd.find("data-block-start=")+18:tmpPd.find("data-block-end=")-2]
+           tmpStart = tmpStart.strip()
+           tmpEnd = tmpPd[tmpPd.find("data-block-end=")+16:tmpPd.find("data-block-order=")-2]
+           tmpEnd = tmpEnd.strip()
+           periodNames.append(dict([('name', name), ('startForm', tmpStart), ('endForm', tmpEnd), ('startInt', timeToInt(tmpStart)), ('endInt', timeToInt(tmpEnd))]))
            table = table[table.find("</tr>")+4:len(table)]
         jsonValues = json.dumps(periodNames)
-        #print(jsonValues)
         return(jsonValues)
 
 class MainPage(webapp2.RequestHandler):
